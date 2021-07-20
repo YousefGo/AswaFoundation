c7595d5ebdfdd5eb17d040dab4397d1336799edd
yousef <yousef0sarkar@gmail.com>
2021-07-20 16:50:06 +0300

    test

--- js/scripts.js
+++ js/scripts.js
@@ -57,3 +57,10 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
