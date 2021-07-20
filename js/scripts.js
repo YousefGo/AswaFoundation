a29fdd65ee4deeaf5b12638aa716542b8bf05204
yousef <yousef0sarkar@gmail.com>
2021-07-20 18:19:36 +0300

    git 2

--- js/scripts.js
+++ js/scripts.js
@@ -58,13 +58,3 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-toggleButton.addEventListener('click', () => {
- 
-  navbarLinks.classList.toggle('active')
-  var x = document.getElementById('logo');
-  x.classList.add('w-100');
-  x.classList.add('text-center');
-})
